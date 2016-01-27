650a7ae38228f8ff911e52db8e07c788a5cbf1ab
oscar <oscar@oscar-PC>
2016-01-27 21:48:17 +0100

    Merge origin/master
    Conflicts:
    	ejemploTablas/src/main/java/com/ejemplotablas/JFrameDatos.java

--- HTML5LESSSASS/Gruntfile.js
+++ HTML5LESSSASS/Gruntfile.js
@@ -3,8 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 module.exports = function (grunt) {
+    
     // Project configuration.
-    grunt.initConfig({
-    });
+grunt.initConfig({
+  'ftp_push': {
+    deploy: {
+      options: {
+		authKey: "23.101.62.6",
+    	host: "23.101.62.6",
+    	dest: "/site/wwwroot",
+    	port: 21
+      },
+      files: [
+        {
+          expand: true,
+          cwd: './public_html',
+          src: [
+            "index.html"
+          ]
+        }
+      ]
+    }
+  }
+});
 };
