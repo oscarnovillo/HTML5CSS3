4902744a1a34166056d37794a82833dd6616aba3
oscar <oscar@oscar-PC>
2016-05-19 21:22:10 +0200

    Merge origin/master
    Conflicts:
    	ejemploTablas/src/main/java/com/ejemplotablas/JFrameDatos.java

---
+++ HTML5Objetos/public_html/js/loginGoogle.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// API 1027953223368-esdmi1edqfr3alk8ei8f4g2o2jjprkr7.apps.googleusercontent.com
+// Secrect aD_MfHDZR_RktaodyTbwkNOR
+//clave de api AIzaSyAldA1QBFthWcpYXtJVPLTxO-gzYRc65WM
+
